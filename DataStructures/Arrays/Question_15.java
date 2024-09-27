9498f2753f6e51516416352bd7a03230359fac3a
Aditya Shriwas <adityashriwas18@gmail.com>
2024-09-27 07:20:57 -0700

    Rotated Sorted Array

---
+++ DataStructures/Arrays/Question_15.java
@@ -0,0 +1,40 @@
+// Rotated Sorted Array
+
+package com.DataStructures.Arrays;
+
+public class Question_15 {
+
+    public static int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4,5,6,7,0,1,2};
+        System.out.println(search(nums, 0));
+    }
+}
