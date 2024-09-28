042270f256ea88fdeeb83330b94b5104d074c7b8
Aditya Shriwas <adityashriwas18@gmail.com>
2024-09-28 07:06:20 -0700

    Star Pattern

--- DataStructures/Patterns/Pattern_1.java
+++ DataStructures/Patterns/Pattern_1.java
@@ -9,6 +9,7 @@ public class Pattern_1 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
+
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
