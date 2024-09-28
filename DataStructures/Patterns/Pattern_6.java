8667ce7b2a9c8bc65cb5d62d456a02bd9b06e11e
Aditya Shriwas <adityashriwas18@gmail.com>
2024-09-28 07:07:24 -0700

    Floyd's Triangle pattern

--- DataStructures/Patterns/Pattern_6.java
+++ DataStructures/Patterns/Pattern_6.java
@@ -5,7 +5,6 @@ package com.DataStructures.Patterns;
 public class Pattern_6 {
     public static void main(String[] args) {
         int num = 5, count = 1;
-
         for (int i = 1; i <= num; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(count+" ");
