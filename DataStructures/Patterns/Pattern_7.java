13958473abc7e1924a9f5ece14cdc169fc582421
Aditya Shriwas <adityashriwas18@gmail.com>
2024-09-28 07:07:35 -0700

    0-1 triangle pattern

--- DataStructures/Patterns/Pattern_7.java
+++ DataStructures/Patterns/Pattern_7.java
@@ -5,7 +5,6 @@ package com.DataStructures.Patterns;
 public class Pattern_7 {
     public static void main(String[] args) {
         int num = 5;
-
         for (int i = 1; i <= num; i++) {
             for (int j = 1; j <= i; j++) {
                 if ( (i+j) % 2 ==0 ){
