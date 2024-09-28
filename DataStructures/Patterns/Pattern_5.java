2ec4a9ecee259b787717c60329c014a8737f03d5
Aditya Shriwas <adityashriwas18@gmail.com>
2024-09-28 07:07:14 -0700

    Inverted half pyramid with the numbers pattern

--- DataStructures/Patterns/Pattern_5.java
+++ DataStructures/Patterns/Pattern_5.java
@@ -8,6 +8,7 @@ public class Pattern_5 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number: ");
         int num = sc.nextInt();
+
         for (int i = 1; i <= num ; i++) {
             for (int j = 1; j <= num-i+1 ; j++) {
                 System.out.print(j+" ");
