e8b57d72b0ad6331693371a1817d40eb30e04ca1
Aditya Shriwas <adityashriwas18@gmail.com>
2024-09-28 07:07:47 -0700

    Butterfly pattern

--- DataStructures/Patterns/Pattern_8.java
+++ DataStructures/Patterns/Pattern_8.java
@@ -18,6 +18,7 @@ public class Pattern_8 {
             }
             System.out.println();
         }
+
         for (int i = num; i >= 1; i--) {
             for (int j = 1; j <= i ; j++) {
                 System.out.print("* ");
