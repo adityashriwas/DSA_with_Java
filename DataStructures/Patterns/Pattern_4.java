52e05a84d013cdccf5b7c087dbfcbc91a5c2b6dd
Aditya Shriwas <adityashriwas18@gmail.com>
2024-09-28 07:06:58 -0700

    INVERTED & ROTATED HALF-PYRAMID pattern

--- DataStructures/Patterns/Pattern_4.java
+++ DataStructures/Patterns/Pattern_4.java
@@ -9,6 +9,7 @@ public class Pattern_4 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number: ");
         int num = sc.nextInt();
+
         for (int i = 1; i <= num; i++) {
             for (int j = 1; j <= num-i ; j++) {
                 System.out.print("  ");
