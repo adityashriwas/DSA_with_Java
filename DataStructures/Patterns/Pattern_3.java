c55bdd9bb0ffb117a88c985d76d455ea5805f8c1
Aditya Shriwas <adityashriwas18@gmail.com>
2024-09-28 07:06:46 -0700

    Hollow Rectangle

--- DataStructures/Patterns/Pattern_3.java
+++ DataStructures/Patterns/Pattern_3.java
@@ -7,9 +7,11 @@ import java.util.Scanner;
 public class Pattern_3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter the Number of row and column: ");
         int row = sc.nextInt();
         int col = sc.nextInt();
+
         for (int i = 1; i <= row; i++) {
             for (int j = 1; j <= col ; j++) {
                 if (i == 1 || i == row || j == 1 || j == col){
