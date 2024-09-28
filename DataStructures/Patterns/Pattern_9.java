83e9746c8bbc35fb4c084713dc11d41c5aaad8eb
Aditya Shriwas <adityashriwas18@gmail.com>
2024-09-28 07:07:57 -0700

    Solid Rhombus pattern

--- DataStructures/Patterns/Pattern_9.java
+++ DataStructures/Patterns/Pattern_9.java
@@ -5,7 +5,6 @@ package com.DataStructures.Patterns;
 public class Pattern_9 {
     public static void main(String[] args) {
         int num = 5;
-
         for (int i = 1; i <= num; i++) {
             for (int j = 1; j <= num-i ; j++) {
                 System.out.print("  ");
