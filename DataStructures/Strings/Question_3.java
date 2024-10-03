f4ecdc60bcfab44a30ae76c6d75977ce50d77e77
Aditya Shriwas <adityashriwas18@gmail.com>
2024-10-03 08:56:15 -0700

    Create Question_3.java

---
+++ DataStructures/Strings/Question_3.java
@@ -0,0 +1,23 @@
+// Remove duplicates
+
+package com.DataStructures.Strings;
+
+public class Question_3 {
+
+    public static int removeDuplicates(int[] nums){
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        int k = removeDuplicates(nums);
+        System.out.println(k);
+    }
+}
