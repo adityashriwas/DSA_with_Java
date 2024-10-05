65d9ff659c24ecffc80dc2c0e321546ba87d5244
Aditya Shriwas <adityashriwas18@gmail.com>
2024-10-05 01:00:12 -0700

    Operators in BIT Manipulations

---
+++ DataStructures/BIT_Manipulation/Operators.java
@@ -0,0 +1,12 @@
+package com.DataStructures.BIT_Manipulation;
+
+public class Operators {
+    public static void main(String[] args) {
+        System.out.println(5 & 6); // Binary AND (&)
+        System.out.println(5 | 6); // Binary OR (|)
+        System.out.println(5 ^ 6); // Binary XOR (^)
+        System.out.println(~0);    // Binary NOT (~)
+        System.out.println(5 << 2);// Binary Left shift (<<)
+        System.out.println(5 >> 2);// Binary Right shift (>>)
+    }
+}
