95dc911585e615438991c20201c44b4c56092450
Aditya Shriwas <adityashriwas18@gmail.com>
2024-10-05 00:57:17 -0700

    Create Clear_ith_bit.java

---
+++ DataStructures/BIT_Manipulation/Clear_ith_bit.java
@@ -0,0 +1,13 @@
+package com.DataStructures.BIT_Manipulation;
+
+public class Clear_ith_bit {
+
+    public static int clear_ith_bit(int n, int i){
+        int bitmask = ~(1<<i);
+        return n & bitmask;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(clear_ith_bit(10, 1));
+    }
+}
