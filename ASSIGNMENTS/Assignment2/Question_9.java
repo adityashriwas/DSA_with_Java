760d7a2e1c66e5d65b38324d89c6802d911dd3d5
Aditya Shriwas <adityashriwas18@gmail.com>
2024-10-06 11:15:56 -0700

    Reverse Integer

---
+++ ASSIGNMENTS/Assignment2/Question_9.java
@@ -0,0 +1,24 @@
+package com.ASSIGNMENTS.Assignment2;
+import java.util.*;
+public class Question_9 {
+
+    public static int reverse(int x) {
+        int num = Math.abs(x);
+        int reverse = 0;
+        while (num != 0 ){
+            int rem = num % 10;
+
+            if (reverse > (Integer.MAX_VALUE - rem)/10){
+                return 0;
+            }
+
+            reverse = reverse * 10 + rem;
+            num = num / 10;
+        }
+        return (x < 0)? (-reverse): reverse;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse(-123));
+    }
+}
