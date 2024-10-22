a523b92968b8cfc96ba5f0ae4e5cd634fb734988
Aditya Shriwas <adityashriwas09@gmail.com>
2024-10-22 15:24:38 +0530

    tiling problem

---
+++ DataStructures/Recursion/Question_11.java
@@ -0,0 +1,23 @@
+package com.DataStructures.Recursion;
+
+public class Question_11  {
+
+    public static int tilingProblem(int n){
+//      basse case
+        if (n == 0 || n ==1) {
+            return 1;
+        }
+
+//      vertical choice
+        int fnm1 = tilingProblem(n-1);
+
+//      horizontal choice
+        int fnm2 = tilingProblem(n-2);
+        int totalways = fnm1 + fnm2;
+        return totalways;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(tilingProblem(2));
+    }
+}
